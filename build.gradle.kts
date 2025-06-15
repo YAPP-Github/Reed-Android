403a670b544c976052085ae026ea8f6b2d8fb824
easyhooon <mraz3068@gmail.com>
2025-06-15 21:23:42 +0900

    chore: 프로젝트 ktlint detekt 설정

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,36 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
+import io.gitlab.arturbosch.detekt.extensions.DetektExtension
+import org.jlleitschuh.gradle.ktlint.KtlintExtension
+
 plugins {
+    alias(libs.plugins.kotlin.detekt)
+    alias(libs.plugins.kotlin.ktlint)
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.compose) apply false
+}
+
+val excludeModules: String? by project
+
+allprojects {
+    if (excludeModules?.split(",")?.contains(project.name) != true) {
+        apply {
+            plugin(rootProject.libs.plugins.kotlin.detekt.get().pluginId)
+            plugin(rootProject.libs.plugins.kotlin.ktlint.get().pluginId)
+        }
+
+        afterEvaluate {
+            extensions.configure<DetektExtension> {
+                parallel = true
+                buildUponDefaultConfig = true
+                toolVersion = libs.versions.kotlin.detekt.get()
+                config.setFrom(files("$rootDir/detekt-config.yml"))
+            }
+
+            extensions.configure<KtlintExtension> {
+                version.set(rootProject.libs.versions.kotlin.ktlint.source.get())
+                android.set(true)
+                verbose.set(true)
+            }
+        }
+    }
 }
