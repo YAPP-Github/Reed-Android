6642363ee0aa4d615ab16092bbe82217a7c12760
easyhooon <mraz3068@gmail.com>
2025-06-19 07:17:49 +0900

    [BOOK-45] chore: AndroidFeatureConventionPlugin 내 필요 의존성 추가
    applyPlugins 블럭내 코드 수정

--- build-logic/src/main/java/AndroidFeatureConventionPlugin.kt
+++ build-logic/src/main/java/AndroidFeatureConventionPlugin.kt
@@ -1,4 +1,9 @@
 import com.ninecraft.booket.convention.applyPlugins
+import com.ninecraft.booket.convention.implementation
+import com.ninecraft.booket.convention.api
+import com.ninecraft.booket.convention.ksp
+import com.ninecraft.booket.convention.project
+import com.ninecraft.booket.convention.libs
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.kotlin.dsl.dependencies
@@ -7,13 +12,18 @@ internal class AndroidFeatureConventionPlugin : Plugin<Project> {
     override fun apply(target: Project) {
         with(target) {
             applyPlugins(
-                "booket-android-library",
-                "booket-android-hilt",
-                "booket-android-library-compose",
+                "booket.android.library",
+                "booket.android.hilt",
+                "booket.android.library.compose",
             )
 
             dependencies {
+                implementation(project(path = ":core:designsystem"))
 
+                implementation(libs.bundles.circuit)
+
+                api(libs.circuit.codegen.annotation)
+                ksp(libs.circuit.codegen.ksp)
             }
         }
     }
