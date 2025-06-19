587a92896b8b1bb8ec8a531b14837baeb9dcc159
easyhooon <mraz3068@gmail.com>
2025-06-19 16:59:53 +0900

    chore: theme Composable 네이밍 변경

--- core/designsystem/src/main/kotlin/com/ninecraft/booket/core/designsystem/theme/Theme.kt
+++ core/designsystem/src/main/kotlin/com/ninecraft/booket/core/designsystem/theme/Theme.kt
@@ -34,7 +34,7 @@ private val LightColorScheme = lightColorScheme(
 )
 
 @Composable
-fun BooketAndroidTheme(
+fun BooketTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
     dynamicColor: Boolean = true,
