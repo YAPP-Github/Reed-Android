0177264db4d6adbfc7cfdb9d5927aec975c73ea1
easyhooon <mraz3068@gmail.com>
2025-06-30 22:44:44 +0900

    [BOOK-74] feat: handleException 함수 도입

---
+++ core/common/src/main/kotlin/com/ninecraft/booket/core/common/utils/HandleException.kt
@@ -0,0 +1,36 @@
+package com.ninecraft.booket.core.common.utils
+
+import com.orhanobut.logger.Logger
+import retrofit2.HttpException
+import java.net.ConnectException
+import java.net.SocketTimeoutException
+import java.net.UnknownHostException
+
+interface ErrorHandlerActions {
+    fun setServerErrorDialogVisible(flag: Boolean)
+    fun setNetworkErrorDialogVisible(flag: Boolean)
+}
+
+fun handleException(exception: Throwable, actions: ErrorHandlerActions) {
+    when (exception) {
+        is HttpException -> {
+            if (exception.code() in 500..511) {
+                actions.setServerErrorDialogVisible(true)
+            } else {
+                exception.message?.let { Logger.e(it) }
+            }
+        }
+
+        is UnknownHostException, is ConnectException -> {
+            actions.setNetworkErrorDialogVisible(true)
+        }
+
+        is SocketTimeoutException -> {
+            actions.setServerErrorDialogVisible(true)
+        }
+
+        else -> {
+            exception.message?.let { Logger.e(it) }
+        }
+    }
+}
