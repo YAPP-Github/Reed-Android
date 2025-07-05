9ed9968a1fca91e3cc53c1347e8e7289be4c6844
easyhooon <mraz3068@gmail.com>
2025-07-05 16:15:18 +0900

    [BOOK-92] feat: 내 서재 화면 진입시 유저 프로필 조회 API 호출되도록 임시 구현

--- feature/library/src/main/kotlin/com/ninecraft/booket/feature/library/LibraryPresenter.kt
+++ feature/library/src/main/kotlin/com/ninecraft/booket/feature/library/LibraryPresenter.kt
@@ -1,12 +1,14 @@
 package com.ninecraft.booket.feature.library
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import com.ninecraft.booket.core.common.utils.handleException
 import com.ninecraft.booket.core.data.api.repository.AuthRepository
+import com.ninecraft.booket.core.data.api.repository.UserRepository
 import com.ninecraft.booket.feature.login.LoginScreen
 import com.orhanobut.logger.Logger
 import com.slack.circuit.codegen.annotations.CircuitInject
@@ -21,7 +23,8 @@ import kotlinx.coroutines.launch
 
 class LibraryPresenter @AssistedInject constructor(
     @Assisted private val navigator: Navigator,
-    private val repository: AuthRepository,
+    private val authRepository: AuthRepository,
+    private val userRepository: UserRepository,
 ) : Presenter<LibraryScreen.State> {
 
     @Composable
@@ -29,6 +32,38 @@ class LibraryPresenter @AssistedInject constructor(
         val scope = rememberCoroutineScope()
         var isLoading by rememberRetained { mutableStateOf(false) }
         var sideEffect by rememberRetained { mutableStateOf<LibraryScreen.SideEffect?>(null) }
+        var nickname by rememberRetained { mutableStateOf("") }
+        var email by rememberRetained { mutableStateOf("") }
+
+        LaunchedEffect(Unit) {
+            scope.launch {
+                try {
+                    isLoading = true
+                    userRepository.getUserProfile()
+                        .onSuccess { user ->
+                            nickname = user.nickname
+                            email = user.email
+                        }
+                        .onFailure { exception ->
+                            val handleErrorMessage = { message: String ->
+                                Logger.e(message)
+                                sideEffect = LibraryScreen.SideEffect.ShowToast(message)
+                            }
+
+                            handleException(
+                                exception = exception,
+                                onServerError = handleErrorMessage,
+                                onNetworkError = handleErrorMessage,
+                                onLoginRequired = {
+                                    navigator.resetRoot(LoginScreen)
+                                },
+                            )
+                        }
+                } finally {
+                    isLoading = false
+                }
+            }
+        }
 
         fun handleEvent(event: LibraryScreen.Event) {
             when (event) {
@@ -40,9 +75,9 @@ class LibraryPresenter @AssistedInject constructor(
                     scope.launch {
                         try {
                             isLoading = true
-                            repository.logout()
+                            authRepository.logout()
                                 .onSuccess {
-                                    repository.clearTokens()
+                                    authRepository.clearTokens()
                                     navigator.resetRoot(LoginScreen)
                                 }
                                 .onFailure { exception ->
@@ -70,6 +105,8 @@ class LibraryPresenter @AssistedInject constructor(
 
         return LibraryScreen.State(
             isLoading = isLoading,
+            nickname = nickname,
+            email = email,
             sideEffect = sideEffect,
             eventSink = ::handleEvent,
         )
