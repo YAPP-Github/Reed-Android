a6bf39389a17a49c9f6f77bc2c8ed2c0cfbc7241
seoyoon <syoonlee513@gmail.com>
2025-07-07 18:35:35 +0900

    [BOOK-61] refactor: Radius 변수 네이밍 변경

--- core/designsystem/src/main/kotlin/com/ninecraft/booket/core/designsystem/theme/Radius.kt
+++ core/designsystem/src/main/kotlin/com/ninecraft/booket/core/designsystem/theme/Radius.kt
@@ -6,14 +6,13 @@ import androidx.compose.ui.unit.dp
 
 @Immutable
 data class ReedRadius(
-    val radiusNone: Dp = 0.dp,
-    val radiusXs: Dp = 4.dp,
-    val radiusSm: Dp = 8.dp,
-    val radiusMd: Dp = 12.dp,
-    val radiusLg: Dp = 16.dp,
-    val radiusXl: Dp = 28.dp,
-    val radius2xl: Dp = 32.dp,
-    val radius3xl: Dp = 48.dp,
-    val radiusFull: Dp = 1000.dp,
+    val none: Dp = 0.dp,
+    val xs: Dp = 4.dp,
+    val sm: Dp = 8.dp,
+    val md: Dp = 12.dp,
+    val lg: Dp = 16.dp,
+    val xl: Dp = 28.dp,
+    val xxl: Dp = 32.dp,
+    val xxxl: Dp = 48.dp,
+    val full: Dp = 1000.dp,
 )
-
