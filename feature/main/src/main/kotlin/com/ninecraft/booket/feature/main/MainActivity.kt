0345b801658b6cf0f4f1b6d558d7084f2d7d8d8e
easyhooon <mraz3068@gmail.com>
2025-07-12 18:36:56 +0900

    [BOOK-106] chore: compose-system-ui-controller, compose-keyboard-state 라이브러리 의존서 추가

--- feature/main/src/main/kotlin/com/ninecraft/booket/feature/main/MainActivity.kt
+++ feature/main/src/main/kotlin/com/ninecraft/booket/feature/main/MainActivity.kt
@@ -4,11 +4,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.DisposableEffect
 import androidx.compose.ui.Modifier
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.ninecraft.booket.core.designsystem.theme.White
 import com.ninecraft.booket.feature.main.component.MainBottomBar
 import com.ninecraft.booket.screens.LoginScreen
 import com.slack.circuit.backstack.rememberSaveableBackStack
@@ -18,6 +21,7 @@ import com.slack.circuit.foundation.NavigableCircuitContent
 import com.slack.circuit.foundation.rememberCircuitNavigator
 import com.slack.circuit.overlay.ContentWithOverlays
 import dagger.hilt.android.AndroidEntryPoint
+import tech.thdev.compose.exteions.system.ui.controller.rememberSystemUiController
 import javax.inject.Inject
 
 @AndroidEntryPoint
@@ -31,8 +35,20 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
 
         setContent {
+            val systemUiController = rememberSystemUiController()
             val backStack = rememberSaveableBackStack(root = LoginScreen)
             val navigator = rememberCircuitNavigator(backStack)
+            val isDarkTheme = isSystemInDarkTheme()
+
+            DisposableEffect(systemUiController) {
+                systemUiController.setSystemBarsColor(
+                    color = White,
+                    darkIcons = !isDarkTheme,
+                    isNavigationBarContrastEnforced = false,
+                )
+
+                onDispose {}
+            }
 
             CircuitCompositionLocals(circuit) {
                 Scaffold(
