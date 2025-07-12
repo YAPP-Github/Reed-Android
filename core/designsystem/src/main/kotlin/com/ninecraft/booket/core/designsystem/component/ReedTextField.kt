e6029e76e9e4993b7bc28c7593c9133feb6bf00e
easyhooon <mraz3068@gmail.com>
2025-07-12 20:22:33 +0900

    [BOOK-106] feat: ReedTextField 구성
    - 실기기로 빌드시 icon 다르게 보이는 문제 해결 필요
    - 키보드내 Backspace 클릭시 모든 텍스트가 한번에 지워지는 문제 해결 필요

---
+++ core/designsystem/src/main/kotlin/com/ninecraft/booket/core/designsystem/component/ReedTextField.kt
@@ -0,0 +1,106 @@
+package com.ninecraft.booket.core.designsystem.component
+
+import androidx.annotation.StringRes
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.text.input.TextFieldLineLimits
+import androidx.compose.foundation.text.input.TextFieldState
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.unit.dp
+import com.ninecraft.booket.core.designsystem.ComponentPreview
+import com.ninecraft.booket.core.designsystem.R
+import com.ninecraft.booket.core.designsystem.theme.ReedTheme
+
+@Composable
+fun ReedTextField(
+    searchTextState: TextFieldState,
+    @StringRes searchTextHintRes: Int,
+    onSearch: (String) -> Unit,
+    modifier: Modifier = Modifier,
+    backgroundColor: Color = ReedTheme.colors.baseSecondary,
+    textColor: Color = ReedTheme.colors.contentPrimary,
+    cornerShape: RoundedCornerShape = RoundedCornerShape(ReedTheme.radius.sm),
+    borderStroke: BorderStroke = BorderStroke(width = 1.dp, color = ReedTheme.colors.borderBrand),
+) {
+    val keyboardController = LocalSoftwareKeyboardController.current
+
+    BasicTextField(
+        state = searchTextState,
+        modifier = Modifier.fillMaxWidth(),
+        keyboardOptions = KeyboardOptions(imeAction = ImeAction.Search),
+        onKeyboardAction = {
+            onSearch(searchTextState.text.toString())
+            keyboardController?.hide()
+        },
+        lineLimits = TextFieldLineLimits.SingleLine,
+        textStyle = ReedTheme.typography.body2Regular.copy(color = textColor),
+        decorator = { innerTextField ->
+            Row(
+                modifier = modifier
+                    .background(color = backgroundColor, shape = cornerShape)
+                    .border(
+                        border = borderStroke,
+                        shape = cornerShape,
+                    )
+                    .padding(vertical = ReedTheme.spacing.spacing3),
+                verticalAlignment = Alignment.CenterVertically,
+            ) {
+                Spacer(modifier = Modifier.width(ReedTheme.spacing.spacing4))
+                Box {
+                    if (searchTextState.text.isEmpty()) {
+                        Text(
+                            text = stringResource(id = searchTextHintRes),
+                            color = ReedTheme.colors.contentTertiary,
+                            style = ReedTheme.typography.body2Regular,
+                        )
+                    }
+                    innerTextField()
+                }
+                Spacer(modifier = Modifier.weight(1f))
+                Icon(
+                    imageVector = ImageVector.vectorResource(R.drawable.ic_search),
+                    contentDescription = "Search Icon",
+                    tint = ReedTheme.colors.contentBrand,
+                )
+                Spacer(modifier = Modifier.width(ReedTheme.spacing.spacing4))
+            }
+        },
+    )
+}
+
+@ComponentPreview
+@Composable
+private fun ReedTextFieldPreview() {
+    ReedTheme {
+        ReedTextField(
+            searchTextState = TextFieldState(),
+            searchTextHintRes = R.string.search_book_hint,
+            onSearch = {},
+            modifier = Modifier
+                .height(46.dp)
+                .fillMaxWidth()
+                .padding(horizontal = 20.dp),
+        )
+    }
+}
