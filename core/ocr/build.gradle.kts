17bfe344b06e729e98d4b6fd850e09f8ae172a1e
seoyoon <syoonlee513@gmail.com>
2025-07-12 20:34:31 +0900

    [BOOK-95] chore: MLKit 의존성 추가

--- core/ocr/build.gradle.kts
+++ core/ocr/build.gradle.kts
@@ -1,8 +1,16 @@
+@file:Suppress("INLINE_FROM_HIGHER_PLATFORM")
+
 plugins {
-    alias(libs.plugins.android.library)
-    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.booket.android.library)
+}
+
+android {
+    namespace = "com.ninecraft.booket.core.ocr"
 }
 
 dependencies {
-
+    implementations(
+        libs.google.mlkit.text.recognition,
+        libs.google.mlkit.text.recognition.korean
+    )
 }
