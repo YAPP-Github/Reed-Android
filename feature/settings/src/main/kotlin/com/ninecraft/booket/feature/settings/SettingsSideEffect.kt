8e82bc75a5bb6da77fcf7e488a4e44a7d891af63
seoyoon <syoonlee513@gmail.com>
2025-07-16 17:29:22 +0900

    [BOOK-147] refactor: LaunchedEffect -> RememberedEffect로 변경

--- feature/settings/src/main/kotlin/com/ninecraft/booket/feature/settings/SettingsSideEffect.kt
+++ feature/settings/src/main/kotlin/com/ninecraft/booket/feature/settings/SettingsSideEffect.kt
@@ -2,8 +2,8 @@ package com.ninecraft.booket.feature.settings
 
 import android.widget.Toast
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.platform.LocalContext
+import com.skydoves.compose.effects.RememberedEffect
 
 @Composable
 internal fun HandleSettingsSideEffects(
@@ -12,7 +12,7 @@ internal fun HandleSettingsSideEffects(
 ) {
     val context = LocalContext.current
 
-    LaunchedEffect(state) {
+    RememberedEffect(state.sideEffect) {
         when (state.sideEffect) {
             is SettingsSideEffect.ShowToast -> {
                 Toast.makeText(context, state.sideEffect.message, Toast.LENGTH_SHORT).show()
