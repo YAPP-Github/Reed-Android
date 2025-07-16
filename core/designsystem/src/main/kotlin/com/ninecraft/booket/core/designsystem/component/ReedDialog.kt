65b3dbfd8dcc3623248b7afdf80ef80e15793f31
seoyoon <syoonlee513@gmail.com>
2025-07-16 12:46:46 +0900

    [BOOK-147] feat: ReedDialog 구현

---
+++ core/designsystem/src/main/kotlin/com/ninecraft/booket/core/designsystem/component/ReedDialog.kt
@@ -0,0 +1,123 @@
+package com.ninecraft.booket.core.designsystem.component
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.window.Dialog
+import com.ninecraft.booket.core.designsystem.component.button.ReedButton
+import com.ninecraft.booket.core.designsystem.component.button.ReedButtonColorStyle
+import com.ninecraft.booket.core.designsystem.component.button.largeButtonStyle
+import com.ninecraft.booket.core.designsystem.theme.ReedTheme
+
+@Composable
+fun ReedDialog(
+    title: String = "",
+    subtext: String? = null,
+    confirmButtonText: String = "",
+    dismissButtonText: String? = null,
+    onConfirmRequest: () -> Unit = {},
+    onDismissRequest: () -> Unit = {},
+) {
+    Dialog(
+        onDismissRequest = {
+            onDismissRequest()
+        },
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(
+                    color = ReedTheme.colors.basePrimary,
+                    shape = RoundedCornerShape(
+                        ReedTheme.radius.lg,
+                    ),
+                )
+                .padding(
+                    start = ReedTheme.spacing.spacing5,
+                    top = ReedTheme.spacing.spacing8,
+                    end = ReedTheme.spacing.spacing5,
+                    bottom = ReedTheme.spacing.spacing5,
+                ),
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
+            Text(
+                text = title,
+                color = ReedTheme.colors.contentPrimary,
+                textAlign = TextAlign.Center,
+                style = ReedTheme.typography.headline1SemiBold,
+            )
+            Spacer(modifier = Modifier.height(ReedTheme.spacing.spacing2))
+            subtext?.let {
+                Text(
+                    text = subtext,
+                    color = ReedTheme.colors.contentSecondary,
+                    textAlign = TextAlign.Center,
+                    style = ReedTheme.typography.body2Medium,
+                )
+            }
+            Spacer(modifier = Modifier.height(ReedTheme.spacing.spacing6))
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+            ) {
+                dismissButtonText?.let {
+                    ReedButton(
+                        onClick = {
+                            onDismissRequest()
+                        },
+                        sizeStyle = largeButtonStyle,
+                        colorStyle = ReedButtonColorStyle.SECONDARY,
+                        modifier = Modifier.weight(1f),
+                        text = dismissButtonText,
+                    )
+                    Spacer(modifier = Modifier.width(ReedTheme.spacing.spacing2))
+                }
+                ReedButton(
+                    onClick = {
+                        onConfirmRequest()
+                    },
+                    sizeStyle = largeButtonStyle,
+                    colorStyle = ReedButtonColorStyle.PRIMARY,
+                    modifier = Modifier.weight(1f),
+                    text = confirmButtonText,
+                )
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun ReedConfirmDialogPreview() {
+    ReedTheme {
+        ReedDialog(
+            title = "Title",
+            subtext = "subtext",
+            confirmButtonText = "확인",
+        )
+    }
+}
+
+@Preview
+@Composable
+fun ReedChoiceDialogPreview() {
+    ReedTheme {
+        ReedDialog(
+            title = "Title",
+            subtext = "subtext",
+            confirmButtonText = "확인",
+            dismissButtonText = "취소",
+        )
+    }
+}
