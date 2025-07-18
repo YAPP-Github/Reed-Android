0efc9e65cdfa3ae1ba82279493d9e859c032000c
easyhooon <mraz3068@gmail.com>
2025-07-18 13:50:54 +0900

    [BOOK-150] feat: BottomNavigation 구성

--- feature/main/src/main/kotlin/com/ninecraft/booket/feature/main/component/MainTab.kt
+++ feature/main/src/main/kotlin/com/ninecraft/booket/feature/main/component/MainTab.kt
@@ -5,7 +5,6 @@ import androidx.annotation.StringRes
 import com.ninecraft.booket.feature.main.R
 import com.ninecraft.booket.screens.HomeScreen
 import com.ninecraft.booket.screens.LibraryScreen
-import com.ninecraft.booket.screens.SearchScreen
 import com.slack.circuit.runtime.screen.Screen
 
 enum class MainTab(
@@ -16,22 +15,15 @@ enum class MainTab(
     val screen: Screen,
 ) {
     HOME(
-        iconResId = R.drawable.ic_home_tab,
-        selectedIconResId = R.drawable.ic_selected_home_tab,
+        iconResId = R.drawable.ic_home,
+        selectedIconResId = R.drawable.ic_selected_home,
         labelResId = R.string.home_label,
         contentDescription = "Home Icon",
         screen = HomeScreen,
     ),
-    SEARCH(
-        iconResId = R.drawable.ic_search_tab,
-        selectedIconResId = R.drawable.ic_selected_search_tab,
-        labelResId = R.string.search_label,
-        contentDescription = "Search Icon",
-        screen = SearchScreen,
-    ),
     LIBRARY(
-        iconResId = R.drawable.ic_library_tab,
-        selectedIconResId = R.drawable.ic_selected_library_tab,
+        iconResId = R.drawable.ic_library,
+        selectedIconResId = R.drawable.ic_selected_library,
         labelResId = R.string.library_label,
         contentDescription = "Library Icon",
         screen = LibraryScreen,
