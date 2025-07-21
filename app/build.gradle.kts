a0ec2998bb560c4ac115721ba6e6b8ebc51cc447
easyhooon <mraz3068@gmail.com>
2025-07-21 09:15:45 +0900

    [BOOK-151] feat: 이전 도서 검색 API 연동 작업시 누락된 coil 이미지 캐싱 관련 설정 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,15 +49,17 @@ dependencies {
 
         projects.feature.home,
         projects.feature.library,
-        projects.feature.main,
         projects.feature.login,
+        projects.feature.main,
+        projects.feature.screens,
         projects.feature.search,
         projects.feature.settings,
 
         libs.androidx.activity.compose,
         libs.androidx.startup,
-        libs.logger,
+        libs.coil.compose,
         libs.kakao.auth,
+        libs.logger,
 
         libs.bundles.circuit,
     )
