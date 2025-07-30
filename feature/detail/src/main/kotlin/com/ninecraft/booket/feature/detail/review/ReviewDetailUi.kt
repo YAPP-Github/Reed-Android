835763ff487d8dd53fca205e8e6b5a65c57ce084
seoyoon <syoonlee513@gmail.com>
2025-07-30 19:42:52 +0900

    [BOOK-168] refactor: 기록 상세 화면 접근제어자 변경 및 content로 분리

--- feature/detail/src/main/kotlin/com/ninecraft/booket/feature/detail/review/ReviewDetailUi.kt
+++ feature/detail/src/main/kotlin/com/ninecraft/booket/feature/detail/review/ReviewDetailUi.kt
@@ -37,7 +37,7 @@ import com.ninecraft.booket.core.designsystem.R as designR
 
 @CircuitInject(ReviewDetailScreen::class, ActivityRetainedComponent::class)
 @Composable
-fun ReviewDetail(
+internal fun ReviewDetail(
     state: ReviewDetailUiState,
     modifier: Modifier = Modifier,
 ) {
@@ -52,96 +52,101 @@ fun ReviewDetail(
             startIconRes = designR.drawable.ic_close,
             startIconDescription = "Close Icon",
             startIconOnClick = {
-                // TODO: 창 종료
+                state.eventSink(ReviewDetailUiEvent.OnBackClicked)
             },
         )
+        ReviewDetailContent(modifier)
+    }
+}
+
+@Composable
+private fun ReviewDetailContent(modifier: Modifier = Modifier) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(White),
+    ) {
+        Row(
+            modifier = modifier
+                .fillMaxWidth()
+                .padding(
+                    horizontal = ReedTheme.spacing.spacing5,
+                    vertical = ReedTheme.spacing.spacing4,
+                ),
+            verticalAlignment = Alignment.CenterVertically,
+        ) {
+            NetworkImage(
+                imageUrl = "",
+                contentDescription = "Book CoverImage",
+                modifier = Modifier
+                    .padding(end = ReedTheme.spacing.spacing4)
+                    .width(46.dp)
+                    .height(68.dp)
+                    .clip(RoundedCornerShape(size = ReedTheme.radius.xs)),
+                placeholder = painterResource(designR.drawable.ic_placeholder),
+            )
+            Column(modifier = Modifier.weight(1f)) {
+                Text(
+                    text = "여름은 오래 그곳에 남아",
+                    color = ReedTheme.colors.contentPrimary,
+                    overflow = TextOverflow.Ellipsis,
+                    maxLines = 1,
+                    style = ReedTheme.typography.body1SemiBold,
+                )
+                Spacer(modifier = Modifier.height(ReedTheme.spacing.spacing1))
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    verticalAlignment = Alignment.CenterVertically,
+                ) {
+                    Text(
+                        text = "미쓰이에 마사시",
+                        color = ReedTheme.colors.contentTertiary,
+                        overflow = TextOverflow.Ellipsis,
+                        maxLines = 1,
+                        style = ReedTheme.typography.label1Medium,
+                        modifier = Modifier.weight(0.7f, fill = false),
+                    )
+                    Spacer(Modifier.width(ReedTheme.spacing.spacing1))
+                    VerticalDivider(
+                        modifier = Modifier.height(14.dp),
+                        thickness = 1.dp,
+                        color = ReedTheme.colors.contentTertiary,
+                    )
+                    Spacer(Modifier.width(ReedTheme.spacing.spacing1))
+                    Text(
+                        text = "비채",
+                        color = ReedTheme.colors.contentTertiary,
+                        overflow = TextOverflow.Ellipsis,
+                        maxLines = 1,
+                        style = ReedTheme.typography.label1Medium,
+                        modifier = Modifier.weight(0.3f, fill = false),
+                    )
+                }
+            }
+        }
+        Spacer(modifier = Modifier.height(ReedTheme.spacing.spacing2))
+        ReedDivider()
+        Spacer(modifier = Modifier.height(ReedTheme.spacing.spacing6))
         Column(
             modifier = Modifier
                 .fillMaxSize()
-                .background(White),
+                .padding(horizontal = ReedTheme.spacing.spacing5),
         ) {
-            Row(
-                modifier = modifier
-                    .fillMaxWidth()
-                    .padding(
-                        horizontal = ReedTheme.spacing.spacing5,
-                        vertical = ReedTheme.spacing.spacing4,
-                    ),
-                verticalAlignment = Alignment.CenterVertically,
-            ) {
-                NetworkImage(
-                    imageUrl = "",
-                    contentDescription = "Book CoverImage",
-                    modifier = Modifier
-                        .padding(end = ReedTheme.spacing.spacing4)
-                        .width(46.dp)
-                        .height(68.dp)
-                        .clip(RoundedCornerShape(size = ReedTheme.radius.xs)),
-                    placeholder = painterResource(designR.drawable.ic_placeholder),
-                )
-                Column(modifier = Modifier.weight(1f)) {
-                    Text(
-                        text = "여름은 오래 그곳에 남아",
-                        color = ReedTheme.colors.contentPrimary,
-                        overflow = TextOverflow.Ellipsis,
-                        maxLines = 1,
-                        style = ReedTheme.typography.body1SemiBold,
-                    )
-                    Spacer(modifier = Modifier.height(ReedTheme.spacing.spacing1))
-                    Row(
-                        modifier = Modifier.fillMaxWidth(),
-                        verticalAlignment = Alignment.CenterVertically,
-                    ) {
-                        Text(
-                            text = "미쓰이에 마사시",
-                            color = ReedTheme.colors.contentTertiary,
-                            overflow = TextOverflow.Ellipsis,
-                            maxLines = 1,
-                            style = ReedTheme.typography.label1Medium,
-                            modifier = Modifier.weight(0.7f, fill = false),
-                        )
-                        Spacer(Modifier.width(ReedTheme.spacing.spacing1))
-                        VerticalDivider(
-                            modifier = Modifier.height(14.dp),
-                            thickness = 1.dp,
-                            color = ReedTheme.colors.contentTertiary,
-                        )
-                        Spacer(Modifier.width(ReedTheme.spacing.spacing1))
-                        Text(
-                            text = "비채",
-                            color = ReedTheme.colors.contentTertiary,
-                            overflow = TextOverflow.Ellipsis,
-                            maxLines = 1,
-                            style = ReedTheme.typography.label1Medium,
-                            modifier = Modifier.weight(0.3f, fill = false),
-                        )
-                    }
-                }
-            }
+            Text(
+                text = stringResource(R.string.review_detail_quote_label),
+                color = ReedTheme.colors.contentPrimary,
+                style = ReedTheme.typography.body1Medium,
+            )
             Spacer(modifier = Modifier.height(ReedTheme.spacing.spacing2))
-            ReedDivider()
+            QuoteBox(quote = "소설가들은 늘 소재를 찾아 떠도는 존재 같지만, 실은 그 반대인 경우가 더 잦다.", page = 99)
             Spacer(modifier = Modifier.height(ReedTheme.spacing.spacing6))
-            Column(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .padding(horizontal = ReedTheme.spacing.spacing5),
-            ) {
-                Text(
-                    text = stringResource(R.string.review_detail_quote_label),
-                    color = ReedTheme.colors.contentPrimary,
-                    style = ReedTheme.typography.body1Medium,
-                )
-                Spacer(modifier = Modifier.height(ReedTheme.spacing.spacing2))
-                QuoteBox(quote = "소설가들은 늘 소재를 찾아 떠도는 존재 같지만, 실은 그 반대인 경우가 더 잦다.", page = 99)
-                Spacer(modifier = Modifier.height(ReedTheme.spacing.spacing6))
-                Text(
-                    text = stringResource(R.string.review_detail_impression_label),
-                    color = ReedTheme.colors.contentPrimary,
-                    style = ReedTheme.typography.body1Medium,
-                )
-                Spacer(modifier = Modifier.height(ReedTheme.spacing.spacing2))
-                ReviewBox(modifier)
-            }
+            Text(
+                text = stringResource(R.string.review_detail_impression_label),
+                color = ReedTheme.colors.contentPrimary,
+                style = ReedTheme.typography.body1Medium,
+            )
+            Spacer(modifier = Modifier.height(ReedTheme.spacing.spacing2))
+            ReviewBox(modifier)
         }
     }
 }
