14dc8296fcd6fb55f902a6de43ca018ab270db9f
easyhooon <mraz3068@gmail.com>
2025-07-31 01:35:01 +0900

    [BOOK-188] feat: BookUpdateBottomSheet 구현

--- feature/detail/src/main/kotlin/com/ninecraft/booket/feature/detail/book/BookDetailUi.kt
+++ feature/detail/src/main/kotlin/com/ninecraft/booket/feature/detail/book/BookDetailUi.kt
@@ -1,16 +1,8 @@
 package com.ninecraft.booket.feature.detail.book
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import com.ninecraft.booket.core.designsystem.ComponentPreview
-import com.ninecraft.booket.core.designsystem.component.button.ReedButton
-import com.ninecraft.booket.core.designsystem.component.button.ReedButtonColorStyle
-import com.ninecraft.booket.core.designsystem.component.button.largeButtonStyle
 import com.ninecraft.booket.core.designsystem.theme.ReedTheme
 import com.ninecraft.booket.core.ui.component.ReedBackTopAppBar
 import com.ninecraft.booket.core.ui.component.ReedFullScreen
@@ -20,7 +12,7 @@ import dagger.hilt.android.components.ActivityRetainedComponent
 
 @CircuitInject(BookDetailScreen::class, ActivityRetainedComponent::class)
 @Composable
-fun BookDetailUi(
+internal fun BookDetailUi(
     state: BookDetailUiState,
     modifier: Modifier = Modifier,
 ) {
@@ -31,47 +23,21 @@ fun BookDetailUi(
 
     ReedFullScreen(modifier = modifier) {
         ReedBackTopAppBar(
-            title = "도서 상세 정보",
+            title = "",
             onBackClick = {
                 state.eventSink(BookDetailUiEvent.OnBackClicked)
             },
         )
-
-        Box(
-            modifier = Modifier.fillMaxSize(),
-            contentAlignment = Alignment.Center,
-        ) {
-            Row(
-                verticalAlignment = Alignment.CenterVertically,
-                horizontalArrangement = Arrangement.spacedBy(ReedTheme.spacing.spacing4),
-            ) {
-                ReedButton(
-                    onClick = {
-                        state.eventSink(BookDetailUiEvent.OnBeforeReadingClick)
-                    },
-                    sizeStyle = largeButtonStyle,
-                    colorStyle = ReedButtonColorStyle.PRIMARY,
-                    text = "읽기 전",
-                )
-                ReedButton(
-                    onClick = {
-                        state.eventSink(BookDetailUiEvent.OnReadingClick)
-                    },
-                    sizeStyle = largeButtonStyle,
-                    colorStyle = ReedButtonColorStyle.PRIMARY,
-                    text = "읽는 중",
-                )
-                ReedButton(
-                    onClick = {
-                        state.eventSink(BookDetailUiEvent.OnCompletedClick)
-                    },
-                    sizeStyle = largeButtonStyle,
-                    colorStyle = ReedButtonColorStyle.PRIMARY,
-                    text = "독서 완료",
-                )
-            }
-        }
+        BookDetailContent(state = state)
     }
+}
+
+@Composable
+internal fun BookDetailContent(
+    state: BookDetailUiState,
+    modifier: Modifier = Modifier,
+) {
+
 }
 
 @ComponentPreview
--- feature/search/src/main/kotlin/com/ninecraft/booket/feature/search/SearchUiState.kt
+++ feature/search/src/main/kotlin/com/ninecraft/booket/feature/search/SearchUiState.kt
@@ -59,24 +59,3 @@ sealed interface SearchUiEvent : CircuitUiEvent {
     data object OnBookRegisterSuccessOkButtonClick : SearchUiEvent
     data object OnBookRegisterSuccessCancelButtonClick : SearchUiEvent
 }
-
-enum class BookStatus(val value: String) {
-    BEFORE_READING("BEFORE_READING"),
-    READING("READING"),
-    COMPLETED("COMPLETED"),
-    ;
-
-    fun getDisplayNameRes(): Int {
-        return when (this) {
-            BEFORE_READING -> R.string.book_status_before
-            READING -> R.string.book_status_reading
-            COMPLETED -> R.string.book_status_completed
-        }
-    }
-
-    companion object Companion {
-        fun fromValue(value: String): BookStatus? {
-            return entries.find { it.value == value }
-        }
-    }
-}
