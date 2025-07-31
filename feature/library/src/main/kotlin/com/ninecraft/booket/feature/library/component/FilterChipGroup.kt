904dcfd459ffdcbe5a0701b82642d70efe91be3c
seoyoon <syoonlee513@gmail.com>
2025-07-31 21:23:43 +0900

    [BOOK-195] fix: 내서재 칩 영역 Row -> LazyRow로 변경

--- feature/library/src/main/kotlin/com/ninecraft/booket/feature/library/component/FilterChipGroup.kt
+++ feature/library/src/main/kotlin/com/ninecraft/booket/feature/library/component/FilterChipGroup.kt
@@ -1,9 +1,12 @@
 package com.ninecraft.booket.feature.library.component
 
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -21,19 +24,18 @@ fun FilterChipGroup(
     onChipClick: (LibraryFilterOption) -> Unit,
     modifier: Modifier = Modifier,
 ) {
-    Row(
+    LazyRow(
         modifier = modifier
             .fillMaxWidth()
             .padding(
-                start = ReedTheme.spacing.spacing5,
                 top = ReedTheme.spacing.spacing3,
-                end = ReedTheme.spacing.spacing5,
                 bottom = ReedTheme.spacing.spacing3,
             ),
+        contentPadding = PaddingValues(horizontal = ReedTheme.spacing.spacing5),
         horizontalArrangement = Arrangement.spacedBy(ReedTheme.spacing.spacing2),
         verticalAlignment = Alignment.CenterVertically,
     ) {
-        filterList.forEach { item ->
+        items(filterList) { item ->
             FilterChip(
                 option = item.option,
                 count = item.count,
