1ae5a5e798259afd515d3efa7e3f7a78c2e7a1f0
seoyoon <syoonlee513@gmail.com>
2025-08-01 18:35:09 +0900

    [BOOK-185] feat: 서비스이용약관, 개인정보처리방침 웹 url 변경

--- core/common/src/main/kotlin/com/ninecraft/booket/core/common/constants/WebViewConstants.kt
+++ core/common/src/main/kotlin/com/ninecraft/booket/core/common/constants/WebViewConstants.kt
@@ -1,6 +1,6 @@
 package com.ninecraft.booket.core.common.constants
 
 enum class WebViewConstants(val url: String, val title: String) {
-    PRIVACY_POLICY("https://www.notion.so/23c7dd1d73f6802aa9a9d2b1d4243b85?source=copy_link", "개인정보처리방침"),
-    TERMS_OF_SERVICE("https://www.notion.so/23c7dd1d73f6805bb475d758d355f4e5?source=copy_link", "이용약관"),
+    PRIVACY_POLICY("https://sites.google.com/view/reed-privacypolicy", "개인정보처리방침"),
+    TERMS_OF_SERVICE("https://sites.google.com/view/reed-termsofuse", "이용약관"),
 }
