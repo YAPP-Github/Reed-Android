c0b348a3aa9fe9d9559aecc10f686cc12c6c2dfd
seoyoon <syoonlee513@gmail.com>
2025-08-06 15:57:40 +0900

    [BOOK-225] chore: 감정 선택 이미지 리소스 변경

--- core/designsystem/src/main/kotlin/com/ninecraft/booket/core/designsystem/EmotionTag.kt
+++ core/designsystem/src/main/kotlin/com/ninecraft/booket/core/designsystem/EmotionTag.kt
@@ -5,14 +5,14 @@ import com.ninecraft.booket.core.designsystem.theme.JoyBgColor
 import com.ninecraft.booket.core.designsystem.theme.JoyTextColor
 import com.ninecraft.booket.core.designsystem.theme.SadnessBgColor
 import com.ninecraft.booket.core.designsystem.theme.SadnessTextColor
-import com.ninecraft.booket.core.designsystem.theme.TensionBgColor
-import com.ninecraft.booket.core.designsystem.theme.TensionTextColor
+import com.ninecraft.booket.core.designsystem.theme.InsightBgColor
+import com.ninecraft.booket.core.designsystem.theme.InsightTextColor
 import com.ninecraft.booket.core.designsystem.theme.WarmthBgColor
 import com.ninecraft.booket.core.designsystem.theme.WarmthTextColor
 
 enum class EmotionTag(val label: String, val bgColor: Color, val textColor: Color, val graphic: Int) {
     WARMTH("따뜻함", WarmthBgColor, WarmthTextColor, R.drawable.img_emotion_warmth),
     JOY("즐거움", JoyBgColor, JoyTextColor, R.drawable.img_emotion_joy),
-    TENSION("긴장감", TensionBgColor, TensionTextColor, R.drawable.img_emotion_tension),
     SADNESS("슬픔", SadnessBgColor, SadnessTextColor, R.drawable.img_emotion_sadness),
+    INSIGHT("깨달음", InsightBgColor, InsightTextColor, R.drawable.img_emotion_insight),
 }
