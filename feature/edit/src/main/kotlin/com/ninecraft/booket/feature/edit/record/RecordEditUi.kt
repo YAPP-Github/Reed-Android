eff679aac476262212c8d7b3f54850df8246bae9
easyhooon <mraz3068@gmail.com>
2025-08-18 10:32:16 +0900

    [BOOK-265] fix: 네비게이션 바 버튼 스타일일때 키보드와 하단 버튼 사이 간격이 벌어지는 문제 해결

--- feature/edit/src/main/kotlin/com/ninecraft/booket/feature/edit/record/RecordEditUi.kt
+++ feature/edit/src/main/kotlin/com/ninecraft/booket/feature/edit/record/RecordEditUi.kt
@@ -8,7 +8,11 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.exclude
+import androidx.compose.foundation.layout.ime
 import androidx.compose.foundation.layout.imePadding
+import androidx.compose.material3.ScaffoldDefaults
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
@@ -58,6 +62,7 @@ internal fun RecordEditUi(
     ReedScaffold(
         modifier = modifier.fillMaxSize(),
         containerColor = White,
+        contentWindowInsets = ScaffoldDefaults.contentWindowInsets.exclude(WindowInsets.ime),
     ) { innerPadding ->
         Column(
             modifier = modifier
