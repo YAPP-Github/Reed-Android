92ac48f6d41b76f288659d742fea14682e7466f6
easyhooon <mraz3068@gmail.com>
2025-10-16 22:26:19 +0900

    [BOOK-353] feat: OCR로 추출한 문장 목록 중 선택한 문장 텍스트 컬러 변경

--- feature/record/src/main/kotlin/com/ninecraft/booket/feature/record/ocr/component/SentenceBox.kt
+++ feature/record/src/main/kotlin/com/ninecraft/booket/feature/record/ocr/component/SentenceBox.kt
@@ -25,7 +25,6 @@ fun SentenceBox(
 ) {
     val bgColor = if (isSelected) ReedTheme.colors.bgTertiary else ReedTheme.colors.bgSecondary
     val borderColor = if (isSelected) ReedTheme.colors.borderBrand else Color.Transparent
-    val textColor = if (isSelected) ReedTheme.colors.contentBrand else ReedTheme.colors.contentPrimary
     val textStyle = if (isSelected) ReedTheme.typography.body1Medium else ReedTheme.typography.body1Regular
 
     Box(
@@ -51,7 +50,7 @@ fun SentenceBox(
     ) {
         Text(
             text = sentence,
-            color = textColor,
+            color = ReedTheme.colors.contentPrimary,
             style = textStyle,
         )
     }
