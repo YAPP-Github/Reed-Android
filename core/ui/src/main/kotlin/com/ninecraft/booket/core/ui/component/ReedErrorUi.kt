e815c1a7cf7976f34df8fa69ecfa68b23ccbaa77
easyhooon <mraz3068@gmail.com>
2025-10-25 01:51:23 +0900

    [BOOK-378] refactor: common 모듈 util, utils 패키지 utils 로 통합

--- core/ui/src/main/kotlin/com/ninecraft/booket/core/ui/component/ReedErrorUi.kt
+++ core/ui/src/main/kotlin/com/ninecraft/booket/core/ui/component/ReedErrorUi.kt
@@ -7,23 +7,17 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.Immutable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
+import com.ninecraft.booket.core.common.utils.ErrorType
 import com.ninecraft.booket.core.designsystem.ComponentPreview
 import com.ninecraft.booket.core.designsystem.component.button.ReedButton
 import com.ninecraft.booket.core.designsystem.component.button.ReedButtonColorStyle
 import com.ninecraft.booket.core.designsystem.component.button.mediumButtonStyle
 import com.ninecraft.booket.core.designsystem.theme.ReedTheme
 import com.ninecraft.booket.core.ui.R
-
-@Immutable
-sealed interface ErrorType {
-    data object NetworkError : ErrorType
-    data object ServerError : ErrorType
-}
 
 @Composable
 fun ReedErrorUi(
