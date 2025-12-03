7eb465a0a8dba6c7d1030ec7b046f10f0c93a4d5
easyhooon <mraz3068@gmail.com>
2025-12-03 15:42:56 +0900

    [BOOK-473] chore: compose stability check success

--- feature/home/src/main/kotlin/com/ninecraft/booket/feature/home/HomeUi.kt
+++ feature/home/src/main/kotlin/com/ninecraft/booket/feature/home/HomeUi.kt
@@ -6,6 +6,7 @@ import android.content.pm.PackageManager
 import android.os.Build
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -219,7 +220,7 @@ internal fun HomeContent(
                                             .size(12.dp)
                                             .padding(3.dp)
                                             .clip(CircleShape)
-                                            .background(color),
+                                            .drawBehind { drawRect(color) },
                                     )
                                 }
                             }
